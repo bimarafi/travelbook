0cbe941b32a9cfa7c690be7e41474f52b823d8ef
bimarafi <bimasukmawijaya@gmail.com>
2017-10-12 20:41:08 +0700

    fix sort by price

--- src/pages/search-result/search-result.ts
+++ src/pages/search-result/search-result.ts
@@ -17,6 +17,7 @@ export class SearchResultPage {
     this.color = this.navParams.get('color');
     this.searchResult = this.navParams.get('result');
     this.isReturn = this.navParams.get('is_return');
+    console.log(this.searchResult.departures.result);
     if (this.isReturn) {
       this.data = {
         subtitle: "Kedatangan",
@@ -26,7 +27,9 @@ export class SearchResultPage {
     } else {
       this.data = {
         subtitle: "Keberangkatan",
-        list: this.searchResult.departures.result,
+        list: this.searchResult.departures.result.sort(function(a, b){
+          return ((parseInt(a.price_value.toString()) === parseInt(b.price_value.toString())) ? 0 : ((parseInt(a.price_value.toString()) > parseInt(b.price_value.toString())) ? 1 : -1))
+        }),
         date: this.searchResult.go_det.formatted_date,
       }
     }
