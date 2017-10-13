c63d75129f1fda734e0168666d4304c219c6fa17
bimarafi <bimasukmawijaya@gmail.com>
2017-10-13 17:39:05 +0700

    add method getFlightData

--- src/interfaces/response/index.ts
+++ src/interfaces/response/index.ts
@@ -1,3 +1,4 @@
 export { ITokenReqResult } from "./token-req-result";
 export { IAirportSearchResults } from "./airport-search-results";
+export { IFlightDataResult } from './flight-data-result'
 export { IFlightSearchResults } from "./flight-search-results";
