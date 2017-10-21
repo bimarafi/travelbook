5a49c09b62b6f471c4aeb6afd9cbe11f2cf59504
bimarafi <bimasukmawijaya@gmail.com>
2017-10-21 16:37:36 +0700

    add some validation

--- src/pages/search-result/search-result.ts
+++ src/pages/search-result/search-result.ts
@@ -1,22 +1,30 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { IFlight, IResponse } from "../../interfaces";
+
+import { SearchResultPageValidator } from '../../validators/search-result-page-validator';
 
 @IonicPage()
 @Component({
   selector: 'page-search-result',
   templateUrl: 'search-result.html',
 })
-export class SearchResultPage {
+export class SearchResultPage extends SearchResultPageValidator {
   color: string;
   title: string = 'Pemesanan';
   isReturn: boolean = false;
   searchResult: IResponse.IFlightSearchResults;
   data: { subtitle: string, list: IFlight[], date: string };
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public alertCtrl: AlertController,
+    public navCtrl: NavController,
+    public navParams: NavParams) {
+
+    super(alertCtrl);
     this.color = this.navParams.get('color');
     this.searchResult = this.navParams.get('result');
     this.isReturn = this.navParams.get('is_return');
+    this.go = this.navParams.get('go');
     if (this.isReturn) {
       this.data = {
         subtitle: "Kedatangan",
@@ -34,6 +42,14 @@ export class SearchResultPage {
   ionViewDidLoad() {
   }
   next(item: IFlight) {
+    if (this.go != undefined) {
+      this.item = item;
+      if (this.isValid()) {
+        console.log('Lanjut Ke Halaman Pemesanan');
+      }
+    } else {
+      console.log('Lanjut Ke Halaman Pemesanan');
+    }
     if (this.searchResult.round_trip && !this.isReturn) {
       this.navCtrl.push(SearchResultPage, { result: this.searchResult, color: this.color, is_return: true, go: item });
     }
