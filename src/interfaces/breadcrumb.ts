c4aa26775382ca421f25346cb6c6c73964a4f71d
bimarafi <bimasukmawijaya@gmail.com>
2017-11-28 17:29:14 +0700

    API hotel detail

---
+++ src/interfaces/breadcrumb.ts
@@ -0,0 +1,25 @@
+export interface IBreadcrumb {
+  business_id: string;
+  business_uri: string;
+  business_name: string;
+  area_id: string;
+  area_name: string;
+  kelurahan_id: string;
+  kelurahan_name: string;
+  kecamatan_id: string;
+  kecamatan_name: string;
+  city_id: string;
+  city_name: string;
+  province_id: string;
+  province_name: string;
+  country_id: string;
+  country_name: string;
+  continent_id: string;
+  continent_name: string;
+  kelurahan_uri: string;
+  kecamatan_uri: string;
+  province_uri: string;
+  country_uri: string;
+  continent_uri: string;
+  star_rating: number;
+}
